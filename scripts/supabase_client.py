00d5be218debf51be0538ef58e7ff5d542c132e1
Chloe Chiang <23cchiang@gmail.com>
2026-06-23 13:31:48 -0400

    Fixing supabase

--- scripts/supabase_client.py
+++ scripts/supabase_client.py
@@ -1,6 +1,8 @@
 from supabase import create_client
 import streamlit as st
 
+print("URL:", st.secrets["supabase_url"])
+
 supabase = create_client(
     st.secrets["supabase_url"],
     st.secrets["supabase_key"]
